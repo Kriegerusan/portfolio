989d29882d3cd89383ab66038b12555b68d02e21
Kriegerusan <kriegergamedev@gmail.com>
2024-07-24 20:06:21 +0200

    contact

--- public/js/clock/index.js
+++ public/js/clock/index.js
@@ -3,7 +3,7 @@ function afficherHorloge() {
     let maintenant = new Date();
     let heureAct = maintenant.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
-    document.getElementById("lblHorloge").innerHTML = "Il est " + heureAct
+    document.getElementById("nixieClock").innerHTML = heureAct;
 
  }
 
